11a53d3a2126820383084b804ff277f117ba8e53
Lmntria <seymur1095@gmail.com>
2022-11-02 20:48:33 +0400

    file reader and file practice

--- main.js
+++ main.js
@@ -1,9 +1,23 @@
 // let inputFile=document.getElementsByTagName("input")
 // console.log(inputFile)
-function showFile(input) {
+// function showFile(input) {
+//   let file = input.files[0];
+//   console.log(file)
+//   console.log(`${file.name}`)
+//   console.log(`${file.lastModified}`)
+//   console.log(`${file.lastModifiedDate}`)
+//   // alert(`File name: ${file.name}`); // e.g my.png
+//   // alert(`Last modified: ${file.lastModified}`); // e.g 1552830408824
+// }
+function readFile(input) {
   let file = input.files[0];
-  console.log(file)
+  let reader = new FileReader();
+  reader.readAsDataURL(file);
 
-  // alert(`File name: ${file.name}`); // e.g my.png
-  // alert(`Last modified: ${file.lastModified}`); // e.g 1552830408824
+  reader.onload = function(event) {
+    let url=event.target.result
+    let imgDiv=document.getElementById("photoSeat")
+    imgDiv.style.backgroundImage=`url(${url})`
+  };
+
 }
